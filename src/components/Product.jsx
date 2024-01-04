0d3fefb7b5ca7b352b866484c7aa435dac83d6b5
sakibsarker <sakib15-3824@diu.edu.bd>
2024-01-05 02:56:13 +0600

    ui for product page 80 done

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,10 +4,14 @@ import {Link}  from 'react-router-dom'
 import Rating from './Rating'
 const Product = ({prduct}) => {
   return (
-    <Card className="my-3 p-3 rounded">
+    <>
         <Link to={`/product/${prduct._id}`}>
             {/* <Card.Img src={prduct.image} variant="top"/> */}
-            <Card.Img src={`http://localhost:5000${prduct.image}`} variant="top"/>
+            <Card.Img  style={{
+                      height: "250px",
+                      width: "250px",
+                      objectFit: "cover",
+                    }} src={`http://localhost:5000${prduct.image}`} variant="top"/>
         </Link>
         <Card.Body>
             <Link to={`/product/${prduct._id}`}>
@@ -15,15 +19,15 @@ const Product = ({prduct}) => {
                     <strong>{prduct.name}</strong>
                 </Card.Title>
             </Link>
-            <Card.Text as="div">
+            {/* <Card.Text as="div">
                 <Rating value={prduct.rating} text={`${prduct.numReviews} reviews`}/>
-            </Card.Text>
+            </Card.Text> */}
             <Card.Text as="h5">
                 ${prduct.price}
             </Card.Text>
            
          </Card.Body>
-    </Card>
+    </>
   )
 }
 
