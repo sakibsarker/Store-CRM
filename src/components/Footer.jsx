88f5a4c7d1d5d8da9ed26049572e2eca8612baaf
sakibsarker <sakib15-3824@diu.edu.bd>
2024-01-08 02:05:39 +0600

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import { Container,Row,Col } from 'react-bootstrap'
-const Footer = () => {
-    const currentYear = new Date().getFullYear()
-  return (
-   <footer>
-    <Container>
-        <Row>
-            <Col className="text-center py-3">
-                <p>Store CRM &copy; {currentYear} Powerd by Sakib Sarker</p>
-                </Col>
-        </Row>
-    </Container>
-   </footer>
-  )
-}
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
-export default Footer
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer style={{ backgroundColor: '#f8f9fa', borderTop: '1px solid #000', padding: '5px 0' }}>
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} className="text-center text-md-start py-3">
+            {/* Social Icons */}
+            <a href="#" className="social-icon">
+              <FaFacebook />
+            </a>
+            <a href="#" className="social-icon">
+              <FaTwitter />
+            </a>
+            <a href="#" className="social-icon">
+              <FaInstagram />
+            </a>
+            <a href="#" className="social-icon">
+              <FaLinkedin />
+            </a>
+          </Col>
+          <Col xs={12} md={6} className="text-center text-md-end py-3">
+            <p>Store CRM &copy; {currentYear} Powered by Sakib Sarker</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
